dc0f16139515785e161f267f22b8980730d7a9b3
Ian McGaunn <ian@mcgaunn.com>
2019-07-29 18:15:15 -0400

    remove message.py as it is unused

--- message.py
+++
@@ -1,6 +0,0 @@
-from collections import namedtuple
-# data is a sequence of bytes. key should be a function of which writer
-# wrote the data. sequence is a monotonically increasing sequence number
-# that is tracked separately for each key. e.g. subsequence for A writers
-# can have independently increasing sequence numbers from the 'B' writers sequence.
-LogMessage = namedtuple('LogMessage', ['key', 'data', 'seq'])
